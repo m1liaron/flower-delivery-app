2666e1a50210be645092376e958fb5b51496cc98
Vlad <114946628+m1liaron@users.noreply.github.com>
2025-09-09 15:33:36 +0300

    feat: changed server url to deployed

--- frontend/src/helpers/axiosInstance.ts
+++ frontend/src/helpers/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: "http://localhost:3000",
+	baseURL: "https://flower-delivery-app-backend.vercel.app/flowers",
 });
 
 export { axiosInstance };
